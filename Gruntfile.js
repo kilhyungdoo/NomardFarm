8af044b89938b199af06870de7100baf99737990
Takanori Matsumoto <matscube@gmail.com>
2016-01-30 16:36:23 +0900

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: ['index.html', 'js/**', 'css/**'],
+      },
+    },
+    connect: {
+      server: {
+        options: {
+            hostname: '*',
+            open: {
+                target: 'http://127.0.0.1:9000'
+            },
+            port: 9000,
+        },
+      },
+    },
+  });
+
+  // plugin
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // task
+  grunt.registerTask('default', ['connect', 'watch']);
+
+};
