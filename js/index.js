fa4b3c4e91617a4d3af8780b3ad813c38292f125
Takanori Matsumoto <matscube@gmail.com>
2016-01-31 10:07:51 +0900

    Fix bug on loading tab

--- js/index.js
+++ js/index.js
@@ -2,33 +2,34 @@ document.addEventListener( 'DOMContentLoaded', function() {
   "use strict";
   // this is parent js
 
-	setTab('./tab.html', 'tab1', "1");
-	setTab('./tab.html', 'tab2', "2");
-	setTab('./tab.html', 'tab3', "3");
-
-
-    var latitude = 35.686533327621
-    var longitude = 139.69192653894
-	document.getElementById('#navTabs1-1').addEventListener('click', function() {
-		setTimeout(function() {
-			console.info('weathre chart render start');
-			renderWeather(latitude, longitude);
-		}, 1000);
+	setTab('./tab.html', 'tab1', "1", function() {
+	  var latitude = 35.686533327621;
+	  var longitude = 139.69192653894;
+		document.getElementById('#navTabs1-1').addEventListener('click', function() {
+			setTimeout(function() {
+				console.info('weathre chart render start');
+				renderWeather(latitude, longitude);
+			}, 1000);
+		});
+		renderWeather(latitude, longitude);
 	});
-	renderWeather(latitude, longitude);
+	setTab('./tab.html', 'tab2', "2", function() {
+
+	});
+	setTab('./tab.html', 'tab3', "3", function() {
+		
+	});
+
+
 
 });
 
-function setTab(htmlURL, tabId, idSuffix) {
-	var html = $.ajax({
-		url: htmlURL,
-		async: false
-	}).responseText;
-	console.info(html);
-
-	html = html.replace(/{{id}}/g, idSuffix);
-
-	document.getElementById(tabId).insertAdjacentHTML('afterbegin', html);
+function setTab(htmlURL, tabId, idSuffix, callBack) {
+	$.get(htmlURL, function(html) {
+		html = html.replace(/{{id}}/g, idSuffix);
+		document.getElementById(tabId).insertAdjacentHTML('afterbegin', html);
+		callBack();
+	});
 }
 
 var labelsArray = []
