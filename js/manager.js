410d80c0f0fb3f2cd6b1b4205783de3e70e23b7d
kilhyungdoo <hyungdookil@gmail.com>
2016-01-31 10:13:57 +0900

    スピードアイコンを動的に変化させる

--- js/manager.js
+++ js/manager.js
@@ -19,7 +19,7 @@ Manager = function(targetDocument, roomId, iframeId) {
 Manager.prototype.vehicleSpeedCallBack = function(vehicleSpeed) {
   var id = this.getId('VehicleSpeed', this.iframeId);
   this.log("vehicle speed changed to: ", vehicleSpeed.speed);
-//  this.targetDocument.getElementById(id).innerHTML = Math.floor(vehicleSpeed.speed /1000);
+  this.targetDocument.getElementById(id).innerHTML = Math.floor(vehicleSpeed.speed /1000) + 'Km/h';
 };
 
 Manager.prototype.engineSpeedCallBack = function(engineSpeed) {
