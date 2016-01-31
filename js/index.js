28c800fb465052b77e95a321b0e7659022d1fddf
Kazuyuki INAGE <inage@mulodo.com>
2016-01-31 14:11:40 +0900

    impl temparature and weather

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ document.addEventListener( 'DOMContentLoaded', function() {
   // this is parent js
 
 	setTab('./tab.html', 'tab1', "1", function() {
-	  var latitude = 35.686533327621;
-	  var longitude = 139.69192653894;
+		var latitude = 35.686533327621;
+		var longitude = 139.69192653894;
       
 		// document.getElementById('#navTabs1-1').addEventListener('click', function() {
 		// 	setTimeout(function() {
@@ -13,6 +13,8 @@ document.addEventListener( 'DOMContentLoaded', function() {
 		// 	}, 1000);
 		// });
 		renderWeather(latitude, longitude);
+		var code = "13"
+		renderTemperatureWeather(code);
 	});
 	setTab('./tab.html', 'tab2', "2", function() {
 		console.info('setTab 2');
@@ -31,4 +33,39 @@ function setTab(htmlURL, tabId, idSuffix, callBack) {
 		document.getElementById(tabId).insertAdjacentHTML('afterbegin', html);
 		callBack();
 	});
+}
+
+// 温度と天気
+function renderTemperatureWeather(code) {
+	getForcastPrefJSONP(code);
+}
+
+function getForcastPrefJSONP(code) {
+	var element = document.createElement("script");
+	element.src = 'http://api.yumake.jp/1.1/forecastPref.php?code=' + code + '&key=0251f12288edabb56b3559173587cc54&format=jsonp&callback=getForcastPrefJSON';
+	document.body.appendChild(element);
+}
+
+function getForcastPrefJSON(data) {
+	console.info('getForcastPrefJSONP success');
+	console.info(data);
+
+    var temperature = data.temperatureStation[0].temperature[0]
+    var weatherName = data.area[0].weather[0]
+	console.info('temp: ' + temperature);
+	console.info('weatherName: ' + weatherName);
+
+    $('#temp-1').text(temperature);
+    if (weatherName == "晴れ") {
+        $('#weather-1').attr("src","img/weather_sunny.png");
+    } else if (weatherName == "くもり") {
+        $('#weather-1').attr("src","img/weather_cloudy.png")
+    } else if (weatherName == "雪") {
+        $('#weather-1').attr("src","img/weather_snow.png")
+    } else if (weatherName == "雷") {
+        $('#weather-1').attr("src","img/weather_thunder.png")
+    } else {
+        $('#weather-1').attr("src","img/weather_rain.png");
+    }
+
 }
